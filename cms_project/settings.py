4309ea84e078bbab568df9b8b31ceda8dde5fdc2
Anshul Verma <54620652+AnshulVerma01@users.noreply.github.com>
2021-12-04 13:58:58 +0530

    update

--- cms_project/settings.py
+++ cms_project/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cms',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'HOST': '4.tcp.ngrok.io',
+        'PORT': '14846',
         'USER': 'root',
         'PASSWORD': '',
     }
